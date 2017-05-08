2d567c1930ba9b50af719f0cc7147ef19c728100
Niclas Lindberg <niclas.lindberg@tictac.se>
2017-05-08 13:59:05 +0200

    WIP

--- src/WebX/Routes/Api/Configurator.php
+++ src/WebX/Routes/Api/Configurator.php
@@ -4,14 +4,28 @@ namespace WebX\Routes\Api;
 
 interface Configurator  {
 
+    /**
+     * Adds a controller namespace to prepend to className when loading controllers.
+     * @param $namespace
+     * @return void
+     */
     public function addCtrlNamespace($namespace);
 
+    /**
+     * Configures behavior of a session
+     * @param null $id
+     * @param int $ttl seconds before discarded
+     * @param bool $httpOnly if only accessible via http
+     * @param null $encryptionKey enctryption key to encrypt the session cookie.
+     * @return void
+     */
     public function configureSession($id=null, $ttl=3600, $httpOnly = false,  $encryptionKey = null);
 
     /**
      * Adds an absolute path to the beginning of the path list.
      * @param $path
      * @param bool $append if true appended else prepended.
+     * @return void
      */
     public function addResourcePath($path,$append = true);
 
