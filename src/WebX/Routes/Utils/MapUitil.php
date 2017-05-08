6c25aadcdddf3862ce5d6c42ff34548e5d80732a
Niclas Lindberg <niclas.lindberg@tictac.se>
2017-05-08 22:32:03 +0200

    WIP

--- src/WebX/Routes/Utils/MapUitil.php
+++ src/WebX/Routes/Utils/MapUitil.php
@@ -2,16 +2,26 @@
 
 namespace WebX\Routes\Utils;
 
+use WebX\Routes\Api\Map;
+use WebX\Routes\Api\WritableMap;
 use WebX\Routes\Impl\MapImpl;
 use WebX\Routes\Impl\WritableMapImpl;
 
 class MapUtil {
     private function __construct() {}
 
+    /**
+     * @param array|null $array
+     * @return Map
+     */
     public static function readable(array $array=null) {
         return new MapImpl($array);
     }
 
+    /**
+     * @param array|null $array
+     * @return WritableMap
+     */
     public static function writable(array $array=null) {
         return new WritableMapImpl($array);
     }
